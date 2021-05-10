09e28d39af64e8fe7445e2cb77267190abb7c6e9
MiranaSGit <omerfarukdemirozu@gmail.com>
2021-05-10 18:07:25 +0300

    10May1807

--- scratch.py
+++ scratch.py
@@ -1,6 +1,15 @@
-import math as m
-import math
-print(math.factorial(5))
+# import math as m
+# print(m.factorial(5))
+# print(m.pi)
+# print(m.log10(1000))
 
+from datetime import date
+from random import choice
+from math import pi, factorial, log10
+if "__name__" == "__main__":
+    print(factorial(5))
+    print(pi)
+    print(log10(1000))
 
-print(dir(m))
+    print(choice(["ankara", "adana", "kaysei", "malatya"]))
+    print(date.today())
