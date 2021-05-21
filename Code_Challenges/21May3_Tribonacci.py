63851e0138b4dcb32d8414a40d1b74440dad4a05
MiranaSGit <omerfarukdemirozu@gmail.com>
2021-05-21 13:37:19 +0300

    21May1337

---
+++ Code_Challenges/21May3_Tribonacci.py
@@ -0,0 +1,29 @@
+def tribonacci(signature, n):
+    if n == 0:
+        return []
+    elif n == 1:
+        return [signature[0]]
+    elif n == 2:
+        return [signature[0], signature[1]]
+    elif n == 3:
+        return signature
+    else:
+        myList = signature
+        count = len(signature)
+        while count < n:
+            myList.append(
+                sum([myList[count-3], myList[count-2], myList[count-1]]))
+            count += 1
+    return myList
+
+
+tribonacci([1, 1, 1], 10)
+tribonacci([0, 0, 1], 10)
+tribonacci([0, 1, 1], 10)
+tribonacci([1, 0, 0], 10)
+tribonacci([0, 0, 0], 10)
+tribonacci([1, 2, 3], 10)
+tribonacci([3, 2, 1], 10)
+tribonacci([1, 1, 1], 1)
+tribonacci([300, 200, 100], 0)
+tribonacci([0.5, 0.5, 0.5], 30)
--- scratch.py
+++ scratch.py
@@ -1,15 +1,19 @@
-# import math as m
-# print(m.factorial(5))
-# print(m.pi)
-# print(m.log10(1000))
+def likes(names):
+    if len(names) == 0:
+        a = "no one likes this"
+        return a
+    elif len(names) == 1:
+        return print(names[0], "likes this")
+    elif len(names) == 2:
+        return print("{} and {} like this".format(names[0], names[1]))
+    elif len(names) == 3:
+        return print("{}, {} and {} like this".format(names[0], names[1], names[2]))
+    else:
+        return print("{}, {} and {} others like this".format(names[0], names[1], len(names)-2))
 
-from datetime import date
-from random import choice
-from math import pi, factorial, log10
-if "__name__" == "__main__":
-    print(factorial(5))
-    print(pi)
-    print(log10(1000))
 
-    print(choice(["ankara", "adana", "kaysei", "malatya"]))
-    print(date.today())
+likes([])
+likes(['Peter'])
+likes(['Jacob', 'Alex'])
+likes(['Max', 'John', 'Mark'])
+likes(['Alex', 'Jacob', 'Mark', "Josph"])
